64331bf2f5f6c7bad2e65495e5fcfdb307c9eb48
Janaina Oliveira <janainapp@gmail.com>
2022-12-17 21:56:37 -0300

    feat req 2 e 3

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,80 @@
+from src.analyze_log import create_lists, separate_clients_info
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+    def __init__(self) -> None:
+        self.__orders = []
+
     def __len__(self):
-        pass
+        return len(self.__orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.__orders.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        days, clients, products = create_lists(self.__orders)
+        clients_info = separate_clients_info(
+            self.__orders, days, clients, products
+        )
+        clients_orders = clients_info[customer]["orders"]
+        order_qnt = 0
+        ordered_prod = ""
+        for order, qnt in clients_orders.items():
+            if qnt > order_qnt:
+                order_qnt = qnt
+                ordered_prod = order
+        return ordered_prod
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        days, clients, products = create_lists(self.__orders)
+        clients_info = separate_clients_info(
+            self.__orders, days, clients, products
+        )
+        clients_orders = clients_info[customer]["orders"]
+        never_ordered = []
+        for order, qnt in clients_orders.items():
+            if not qnt:
+                never_ordered.append(order)
+        return set(never_ordered)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        days, clients, products = create_lists(self.__orders)
+        clients_info = separate_clients_info(
+            self.__orders, days, clients, products
+        )
+        clients_days = clients_info[customer]["days"]
+        never_visited = []
+        for order, qnt in clients_days.items():
+            if not qnt:
+                never_visited.append(order)
+        return set(never_visited)
 
     def get_busiest_day(self):
-        pass
+        days = {}
+        for _, _, day in self.__orders:
+            if day in days:
+                days[day] += 1
+            else:
+                days[day] = 1
+        busiest = ""
+        clients_qnt = 0
+        for day, qnt in days.items():
+            if qnt > clients_qnt:
+                clients_qnt = qnt
+                busiest = day
+        return busiest
 
     def get_least_busy_day(self):
-        pass
+        days = {}
+        for _, _, day in self.__orders:
+            if day in days:
+                days[day] += 1
+            else:
+                days[day] = 1
+        busiest = ""
+        clients_qnt = 50
+        for day, qnt in days.items():
+            if qnt < clients_qnt:
+                clients_qnt = qnt
+                busiest = day
+        return busiest
